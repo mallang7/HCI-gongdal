a1f5ad999d38dd6665f7c61bdee181c5922e75f5
gimkuku <52443092+gimkuku@users.noreply.github.com>
2021-05-27 14:37:03 +0900

    html로 수정 + index.js 수정

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const Router = require('express-router');
 const app = express();
-
+var cons = require('consolidate');
 const router = express.Router();
 const port = 3002;
 
+app.engine('html', cons.swig)
 app.set('views','views'); // 정적 파일 위치 지정
-app.set('view engine','ejs'); // 템플릿 엔진 세팅
+app.set('view engine','html'); // 템플릿 엔진 세팅
 app.use(express.static('statics'));
 
 //main page
 //page1
 app.get('/main', (req, res) => {
-  return res.render("page1.ejs");
+  return res.render("page1.html");
 })
 //page2
 app.get('/schedule', (req, res) => {
-  return res.render("page2.ejs");
+  return res.render("page2.html");
 })
 //page3
 app.get('/schedule/month', (req, res) => {
-  return res.render("page3.ejs");
+  return res.render("page3.html");
 })
 //page4
 app.get('/contest', (req, res) => {
-  return res.render("page4.ejs");
+  return res.render("page4.html");
 })
 //page5
 app.get('/portfolio', (req, res) => {
-  return res.render("page5.ejs");
+  return res.render("page5.html");
 })
 
 app.listen(3000)
